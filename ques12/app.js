285b897c98e4e4c8abd49e0df959f6bf5d1d0dec
Istuti Chhabra <102655463+istutichhabra1@users.noreply.github.com>
2025-04-23 20:56:57 +0530

    Create app.js

---
+++ ques12/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>My Tech Stack</h1>
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
